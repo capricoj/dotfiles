7fc01692dfbad567269a79fb7f58300308d464a6
math <yllxxq@163.com>
2024-03-21 13:20:47 +0800

    new

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+set EDITOR nvim
+alias vim='nvim'
+alias vi='nvim'
+alias svim='sudo nvim'
+export SUDO_EDITOR="nvim"
